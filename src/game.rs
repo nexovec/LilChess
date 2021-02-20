222a21bf6f6b591e2abe74ef7fc7d22237a20c7c
nexovec <hracceky@gmail.com>
2021-02-20 17:10:39 +0100

    broken dialog boxes

--- src/game.rs
+++ src/game.rs
@@ -2,7 +2,7 @@
 use std::{borrow::{Borrow, BorrowMut}, ops::{Index, IndexMut}};
 
 use graphics::{Rectangle, Texture};
-use tetra::{Context, graphics::{self, Color, text::{Text, VectorFontBuilder}}, math::Vec2};
+use tetra::{Context, graphics::{self, Color, text::{Text, VectorFontBuilder}}, math::{Vec2, Vec4}};
 pub enum Transition{
     Push(Box<dyn Scene>),
     Pop,
@@ -29,8 +29,28 @@ impl MenuScene{
         };
         Ok(temp)
     }
-    fn get_hovered_menu_item_index(&mut self)->Option<i32>{
-        Some(0)
+    fn get_hovered_menu_item_index(&mut self,ctx: &mut Context)->Option<i32>{
+        let x_offset = 800/2-30;
+        let y_offset= 260;
+        let x_border = 10;
+        let y_border = 1;
+        let y_spacing = 10;
+
+        let x_coord = x_offset-x_border;
+        for i in 0..self.text_options.len(){
+            let dims = self.text_options.index_mut(i).get_bounds(ctx).unwrap();
+            let y_tex = dims.height as i32+y_border*2;
+            let x_tex = dims.width as i32+x_border*2;
+            let y_coord = y_offset-y_border+i as i32*(dims.width as i32+y_border*2);
+            if self.is_mouse_hovered_over_rectangle(ctx, Vec4::new(x_coord as f32,y_coord as f32,x_tex as f32,y_tex as f32)){
+                return Some(i as i32);
+            }
+        }
+        None
+    }
+    fn is_mouse_hovered_over_rectangle(&mut self, ctx:&Context, dim: Vec4<f32>)->bool{
+        let mp = tetra::input::get_mouse_position(ctx);
+        mp.x>=dim.x && mp.x<dim.x+dim.z && mp.y>=dim.y && mp.y <dim.w
     }
 }
 impl Scene for MenuScene{
@@ -49,11 +69,9 @@ impl Scene for MenuScene{
         let box_a = (255.0*0.8) as u8;
         let mut dim:Rectangle;
         graphics::clear(ctx, self.bcg_color);
-        #[allow(irrefutable_let_patterns)]
-        if let item = self.get_hovered_menu_item_index(){
+        if let Some(item) = self.get_hovered_menu_item_index(ctx){
             //TODO: draw a rectangle around hovered UI item
-            let i = item.unwrap();
-            dim = self.text_options.index_mut(item.unwrap() as usize).get_bounds(ctx).unwrap();
+            dim = self.text_options.index_mut(item as usize).get_bounds(ctx).unwrap();
             let y_tex = dim.height as i32+y_border*2;
             let x_tex = dim.width as i32+x_border*2;
             // FIXME: boo, slow!
@@ -61,7 +79,7 @@ impl Scene for MenuScene{
             .take(x_tex as usize*y_tex as usize).flatten().copied().collect::<Vec<u8>>();
             let tex = Texture::from_rgba(ctx,x_tex,y_tex,&pixels);
             let x_coord = x_offset-x_border;
-            let y_coord = y_offset-y_border+i*(dim.width as i32+y_border*2);
+            let y_coord = y_offset-y_border+item*(dim.width as i32+y_border*2);
             tex.unwrap().draw(ctx, Vec2::<f32>::new(x_coord as f32,y_coord as f32));
         }
         for i in 0..self.text_options.len(){
