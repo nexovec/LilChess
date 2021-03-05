49ad270e77f1c16b6213e674b17017329aab73cb
nexovec <hraccesky@gmai.com>
2021-03-05 23:00:16 +0100

    Working MenuScene

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
 use tetra::{Context, graphics::{Color, text::{Text, VectorFontBuilder}}, math::Vec2};
+use tetra::graphics;
 use crate::ui::MenuButton;
 
 pub enum Transition{
@@ -19,24 +20,33 @@ impl MenuScene {
     pub fn new(ctx: &mut Context)->tetra::Result<MenuScene>{
         let font = VectorFontBuilder::new("./res/Exo2-regular.otf")?;
         let size = 32.0;
+        let borders = Vec2::new(18,18);
 
-        let borders1 = Vec2::new(10,10);
-        let pos1 = Vec2::new(10,10);
-        let text1 = Text::new("Quit nub", font.with_size(ctx, size)?);
-        let func1 = Box::new(||{Transition::Pop});
-        let btn1 = MenuButton::new(borders1,pos1,text1,func1);
+        let pos1 = Vec2::new(300,200);
+        let text1 = Text::new("New Game", font.with_size(ctx,size)?);
+        let func1 = Box::new(||{Transition::Push(Box::new(GameScene::new()))});
+        let btn1 = MenuButton::new(borders,pos1,text1,func1);
+
+        let pos2 = Vec2::new(0,70)+pos1;
+        let text2 = Text::new("Quit nub", font.with_size(ctx, size)?);
+        let func2 = Box::new(||{Transition::Pop});
+        let btn2 = MenuButton::new(borders,pos2,text2,func2);
+
         Ok(MenuScene{
             bcg_color: Color::rgb(0.2,0.8,0.4),
-            buttons: vec![btn1]
+            buttons: vec![btn1,btn2]
         })
     }
 }
 // TODO: refactor
 impl Scene for MenuScene{
     fn draw(&mut self, ctx: &mut Context)->Transition {
+        graphics::clear(ctx, self.bcg_color);
         for i in self.buttons.iter_mut(){
+            // TODO: use option instead
             match i.draw(ctx){
                 Transition::Pop=>return Transition::Pop,
+                Transition::Push(s)=>return Transition::Push(s),
                 _=>continue
             }
         }
@@ -47,9 +57,25 @@ impl Scene for MenuScene{
         for i in self.buttons.iter_mut(){
             match i.update(ctx){
                 Transition::Pop=>return Transition::Pop,
+                Transition::Push(s)=>return Transition::Push(s),
                 _=>continue
             }
         }
+        Transition::None
+    }
+}
+struct GameScene;
+impl GameScene{
+    fn new()->GameScene{
+        GameScene
+    }
+}
+impl Scene for GameScene{
+    fn draw(&mut self, ctx:&mut Context)->Transition{
+        graphics::clear(ctx, Color::BLUE);
+        Transition::None
+    }
+    fn update(&mut self, ctx: &mut Context)->Transition{
         Transition::None
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,8 @@
+use tetra::graphics::Texture;
 use std::{borrow::{Borrow, BorrowMut}, process::exit};
 
 use tetra::{Context, graphics::text::Text, input::MouseButton, math::Vec2};
+// TODO: Research Vec2.as_()
 
 use crate::game::{self, Scene, Transition};
 pub struct MenuButton{
@@ -22,7 +24,7 @@ impl MenuButton{
 impl UIMouseInteractableRect for MenuButton{
     fn check_mouse_interaction(&mut self, ctx: &mut Context)->Transition {
         let temp = self.text.get_bounds(ctx).unwrap();
-        if self.is_hovered(ctx, self.pos.borrow(), Vec2::<f32>::new(temp.width, temp.height)){
+        if self.is_hovered(ctx, self.pos.borrow(), Vec2::<f32>::new(temp.width, temp.height)+(self.borders*2).as_()){
             if tetra::input::is_mouse_button_pressed(ctx, MouseButton::Left){
                 return (*self.on_click)();
             }
@@ -32,7 +34,15 @@ impl UIMouseInteractableRect for MenuButton{
 }
 impl Scene for MenuButton{
     fn draw(&mut self, ctx: &mut Context)->Transition {
-        self.text.draw(ctx,self.pos.as_());
+        let temp = self.text.get_bounds(ctx).unwrap();
+        let size: Vec2<f32> = (self.borders*2).as_() + Vec2::new(temp.width,temp.height);
+        if self.is_hovered(ctx, &self.pos, size){
+            let data = std::iter::repeat(&[160,160,160,200]).take(size.x as usize*size.y as usize)
+            .flatten().copied().collect::<Vec<u8>>();
+            let texture = Texture::from_rgba(ctx, size.x as i32, size.y as i32, &data);
+            texture.unwrap().draw(ctx, self.pos.as_());
+        }
+        self.text.draw(ctx,(self.pos + self.borders).as_());
         Transition::None
     }
 
