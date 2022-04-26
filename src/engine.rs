e7d235ee35a76e992ca32f0ab8a5429df2877b8e
nexovec <hraccesky@gmail.com>
2022-04-27 01:01:26 +0200

    better static position scoring

--- src/engine.rs
+++ src/engine.rs
@@ -2,7 +2,6 @@ use crate::game_types::*;
 use std::sync::mpsc;
 use std::sync::mpsc::Sender;
 use std::thread;
-use std::time;
 pub struct Engine {
     sender: mpsc::Sender<ChessMove>,
     receiver: mpsc::Receiver<ChessMove>,
@@ -22,9 +21,9 @@ impl Engine {
             computing_thread_handle: None,
         }
     }
-    fn static_position_evaluation(board: &BoardState) -> f32 {
+    fn static_position_evaluation(board: &BoardState, mv: &ChessMove) -> f32 {
         let mut result = 0.0f32;
-        // TODO: precompute MoveDescription per move
+        // TODO: Precompute MoveDescription per move
         if board.evaluate_is_check(None) {
             result += board.score_for_current_player(-1.0f32);
         }
@@ -50,61 +49,81 @@ impl Engine {
         };
         result
     }
-    fn compute_position_score(board_state: &BoardState, depth: u32) -> f32 {
+    fn compute_position_score(board_state: &BoardState, mv: &ChessMove, depth: u32) -> f32 {
+        // TODO: Pick the maximum if white to move, minimum if black to move
+        // TODO: Get rid of Engine::compute_move()
+        // TODO: Track checks past depth
+        // TODO: Track capture sequences past depth
+        // TODO: Track mate threats past depth
+        // TODO: Track capture threats past depth
         let moves = board_state.get_all_legal_moves();
         if moves.len() == 0 {
-            return Engine::static_position_evaluation(board_state);
+            return Engine::static_position_evaluation(board_state, mv);
         }
         if depth == 1 {
             let best_move = moves
                 .iter()
                 .min_by(|mv1, mv2| {
                     let static_position_score_1 =
-                        Engine::static_position_evaluation(&board_state.after_move(*mv1));
+                        Engine::static_position_evaluation(&board_state.after_move(*mv1), mv1);
                     let static_position_score_2 =
-                        Engine::static_position_evaluation(&board_state.after_move(*mv2));
+                        Engine::static_position_evaluation(&board_state.after_move(*mv2), mv2);
                     static_position_score_1
                         .partial_cmp(&static_position_score_2)
                         .unwrap()
                 })
                 .unwrap();
-            return Engine::static_position_evaluation(&board_state.after_move(best_move));
+            return Engine::static_position_evaluation(
+                &board_state.after_move(best_move),
+                best_move,
+            );
         }
         let subnode_scores = moves
             .iter()
             .map(|mv| {
                 let new_position = board_state.after_move(mv);
-                Engine::compute_position_score(&new_position, depth - 1)
+                Engine::compute_position_score(&new_position, mv, depth - 1)
             })
             .collect::<Vec<_>>();
-        *subnode_scores
-            .iter()
-            .min_by(|a, b| a.partial_cmp(b).unwrap())
-            .unwrap()
-    }
-    fn compute_move(board_state: BoardState, tx: Sender<ChessMove>) {
-        // thread::sleep(time::Duration::from_millis(1500));
-        let moves = board_state.get_all_legal_moves();
-        // FIXME: iterator performance is oof.
-        let depth = 2;
-        // let best_move = moves.last().unwrap();
-        let best_move = moves
-            .iter()
-            .min_by(|mv1, mv2| {
-                let score1 = Engine::compute_position_score(&board_state.after_move(*mv1), depth);
-                let score2 = Engine::compute_position_score(&board_state.after_move(*mv2), depth);
-                score1.partial_cmp(&score2).unwrap()
-            })
-            .unwrap()
-            .to_owned();
-        let _ = tx.send(best_move);
-        drop(tx);
+        if board_state.player_to_move == PlayerColor::WHITE {
+            return *subnode_scores
+                .iter()
+                .max_by(|a, b| a.partial_cmp(b).unwrap())
+                .unwrap();
+        } else {
+            return *subnode_scores
+                .iter()
+                .min_by(|a, b| a.partial_cmp(b).unwrap())
+                .unwrap();
+        }
     }
     pub fn maybe_calculate_move(&mut self, board_state: BoardState) -> Option<ChessMove> {
         if self.computing_thread_handle.is_none() {
             let tx = self.sender.clone();
             self.computing_thread_handle = Some(thread::spawn(move || {
-                Engine::compute_move(board_state, tx);
+                // thread::sleep(time::Duration::from_millis(1500));
+                let moves = board_state.get_all_legal_moves();
+                let depth = 2;
+                // let best_move = moves.last().unwrap();
+                let best_move = moves
+                    .iter()
+                    .min_by(|mv1, mv2| {
+                        let score1 = Engine::compute_position_score(
+                            &board_state.after_move(*mv1),
+                            mv1,
+                            depth,
+                        );
+                        let score2 = Engine::compute_position_score(
+                            &board_state.after_move(*mv2),
+                            mv2,
+                            depth,
+                        );
+                        score1.partial_cmp(&score2).unwrap()
+                    })
+                    .unwrap()
+                    .to_owned();
+                let _ = tx.send(best_move);
+                drop(tx);
             }));
         }
         let maybe_result = self.receiver.try_recv();
