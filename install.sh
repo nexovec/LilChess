b5acde3a22ec1d54221f664002eb05d1ec78f0c7
nexovec <hraccesky@gmail.com>
2022-05-15 23:27:53 +0200

    linux build script

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt upgrade
+sudo apt update
+sudo apt install build-essential
+sudo apt install curl
+sudo apt install libsdl2-dev
+sudo apt install libasound2-dev
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
